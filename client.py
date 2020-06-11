a0850fc54cde2d23ec3a71903e9f3894a4e158ca
Ronald <rnor3536@uni.sydney.edu.au>
2020-06-11 18:50:48 +1000

    Streaming the kmeans datapoints with timestamps

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
-import socket #import socket module
+import socket
+import time
+from datetime import datetime
 
 s = socket.socket() #create a socket object
 host = socket.gethostname() #Host i.p
@@ -6,9 +8,16 @@ port = 12397 #Reserve a port for your service
 
 s.connect((host, port))
 
+input = []
+
 while True:
     data = s.recv(1024)
-    print(repr(data))
+    # print(data.decode("utf-8"))
+    if data == b"":
+        break
+    input.append(data.decode("utf-8")+','+str(datetime.now()))
+    print(data.decode("utf-8")+','+str(datetime.now()))
+s.close()
 #
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 #     s.connect((host, port))
--- server.py
+++ server.py
@@ -1,24 +1,36 @@
-### Server to publish time
-
-import socket
-from datetime import datetime
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
 import time
+from datetime import datetime
+import socket #import socket module
+
+def create_data(n_samples, n_features, centers, std):
+    features, target = make_blobs(n_samples = n_samples,
+                                  # two feature variables,
+                                  n_features = n_features,
+                                  # four clusters,
+                                  centers = centers,
+                                  # with .65 cluster standard deviation,
+                                  cluster_std = std,
+                                  # shuffled,
+                                  shuffle = True)
+    return features, target
+
+features, target = create_data(100,3,8,0.65)
 
 s = socket.socket() #Create a socket object
 host = socket.gethostname() #Get the local machine name
 port = 12397 # Reserve a port for your service
 s.bind((host,port)) #Bind to the port
-counter = 10**2
 
 s.listen() #Wait for the client connection
 while True:
     c,addr = s.accept() #Establish a connection with the client
     print("Got connection from"+ str(addr))
-    for i in range(counter):
-        message = str(datetime.now())
+    for i in range(len(features)):
+        message = str(datetime.now())+','+ str(features[i])+','+str(target[i])
         print(f'Send: {message!r}')
         c.send(message.encode())
-        time.sleep(1)
     c.close()
 
 
